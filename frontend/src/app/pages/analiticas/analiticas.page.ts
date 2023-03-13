ff8006d9012135e9d358b48fc2c18f0b32b6d475
javi-11 <javiterrobaorozco@gmail.com>
2023-03-13 22:54:52 +0100

    Versión inicial finalizada

--- frontend/src/app/pages/analiticas/analiticas.page.ts
+++ frontend/src/app/pages/analiticas/analiticas.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
 import { AnaliticasService } from 'src/app/services/analiticas.service';
+import Chart from 'chart.js/auto';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-analiticas',
@@ -8,6 +10,11 @@ import { AnaliticasService } from 'src/app/services/analiticas.service';
   styleUrls: ['./analiticas.page.scss'],
 })
 export class AnaliticasPage implements OnInit {
+  
+  @ViewChild('barChart') barChart:any;
+
+  bars: any;
+  colorArray: any;
   //BORRAR eso más adelante cuando ya estén las llamadas
   analiticas = [
     {
@@ -41,12 +48,61 @@ export class AnaliticasPage implements OnInit {
     },
     {
       id: 3,
-      date: '16:34 Jueves, 23/03/2023',
-      value: '80',
-      metric: 2,
+      date: '16:34 Jueves, 28/03/2023',
+      value: '56',
+      metric: 1,
     },
     {
-      id: 3,
+      id: 4,
+      date: '17:00 Viernes, 29/03/2023',
+      value: '70',
+      metric: 1,
+    },
+    {
+      id: 5,
+      date: '16:34 Jueves, 30/03/2023',
+      value: '60',
+      metric: 1,
+    },
+    {
+      id: 6,
+      date: '17:00 Lunes, 31/03/2023',
+      value: '56',
+      metric: 1,
+    },
+    {
+      id: 7,
+      date: '16:34 Jueves, 01/04/2023',
+      value: '58',
+      metric: 1,
+    },
+    {
+      id: 8,
+      date: '17:00 Lunes, 03/04/2023',
+      value: '60',
+      metric: 1,
+    }, 
+    {
+      id: 8,
+      date: '16:34 Jueves, 05/04/2023',
+      value: '57',
+      metric: 1,
+    },
+    {
+      id: 9,
+      date: '17:00 Lunes, 06/04/2023',
+      value: '55',
+      metric: 1,
+    },
+
+    {
+      id: 10,
+      date: '16:34 Jueves, 07/04/2023',
+      value: '50',
+      metric: 1,
+    },
+    {
+      id: 4,
       date: '18:47 Lunes, 27/03/2023',
       value: '79',
       metric: 2,
@@ -58,6 +114,11 @@ export class AnaliticasPage implements OnInit {
   //Se ejecuta al crear la página por parte de angular
   ngOnInit() {
     this.loadAnaliticas();
+
+  }
+
+  ionViewDidEnter() {
+    this.createChart(1);
   }
 
   async loadAnaliticas(){
@@ -78,5 +139,39 @@ export class AnaliticasPage implements OnInit {
     */
     console.log(this.analiticasService.getAnaliticas);
     console.log(this.analiticas)
+    
+  }
+  createChart(id:any){
+
+    const dates: String[] = [];
+    const values: String[] = [];
+    for( const medicion of this.mediciones){
+      if(medicion.metric == id){
+
+        const aux = medicion.date.split(" ");
+        console.log(aux);
+        dates.push(aux[2]);
+        values.push(medicion.value)
+      }
+      
+    }
+    Chart.defaults.color = 'black';
+    Chart.defaults.backgroundColor = '#f4f5f9';
+
+    const mensaje: string='Resultados de la analítica('+this.analiticas[id].unit+')'
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: 'line',
+      data: {
+        labels: dates,
+        datasets: [{
+          label: mensaje,
+          data: values,
+          backgroundColor: "light", // array should have same number of elements as number of dataset
+          borderColor: "light", // array should have same number of elements as number of dataset
+          
+          borderWidth: 1
+        }]
+      }
+    });
   }
 }
