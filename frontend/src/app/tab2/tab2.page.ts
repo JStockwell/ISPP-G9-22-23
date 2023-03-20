571c07a37fa9658eba860ee3bcf95d81edc3d1bb
alvvegrod <80700847+alvvegrod@users.noreply.github.com>
2023-03-20 21:28:34 +0100

    Fix: Correción mostrar diario emocional (emoji)

--- frontend/src/app/tab2/tab2.page.ts
+++ frontend/src/app/tab2/tab2.page.ts
@@ -31,12 +31,95 @@ async loadEntradasDiarioEmocional(){
 
   this.diarioEmocionalService.getDiarioEmocional().subscribe((res)=> {
     loading.dismiss();
-    this.entries =[res];
+    this.entries =res;
     console.log(this.entries)
     console.log("Diario Emocional")
   });
 
 }
+
+
+
+
+getImagenEstado(imagen:string): any {
+  if(imagen == "G" ){
+    return "/assets/images/cara-feliz.png";
+  }
+  if(imagen == "VG" ){
+    return "/assets/images/cara-muy-feliz.png";
+  }
+  
+  if(imagen == "S" ){
+    return "/assets/images/cara-neutral.png";
+  }
+  
+  if(imagen == "B" ){
+    return "/assets/images/cara-triste.png";
+  }
+  
+  if(imagen == "VB" ){
+    return "/assets/images/cara-muy-triste.png";
+  }
+  
+
+  
+}
+
+getImagenComida(imagen:string): any {
+  if(imagen == "HEALTHY" ){
+    return "/assets/images/salad.png";
+  }
+  if(imagen == "FAST" ){
+    return "/assets/images/hamburger.png";
+  }
+  
+  if(imagen == "NONE" ){
+    return "/assets/images/plato.png";
+  }
+  
+
+  
+
+}
+
+getImagenSuenyo(imagen:string): any {
+  if(imagen == "DEEP" ){
+    return "/assets/images/slumber.png";
+  }
+  if(imagen == "LIGHT" ){
+    return "/assets/images/sleeping.png";
+  }
+  
+  if(imagen == "NONE" ){
+    return "/assets/images/hotel-bed.png";
+  }
+  
+
+}
+
+getImagenTiempo(imagen:string): any {
+  if(imagen == "SUNNY"){
+    return "/assets/images/dom.png";
+  }
+  if(imagen == "CLOUDY" ){
+    return "/assets/images/nubes.png";
+  }
+  
+  if(imagen == "RAINY" ){
+    return "/assets/images/lluvia.png";
+  }
+  
+  if(imagen == "STORMY" ){
+    return "/assets/images/tormenta.png";
+  }
+  
+  if(imagen == "SNOWY" ){
+    return "/assets/images/nieve.png";
+  }
+  
+
+}
+
  
 }
 
