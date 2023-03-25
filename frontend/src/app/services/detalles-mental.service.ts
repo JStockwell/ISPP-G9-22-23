f3a64da96b6311e3747f006cff82f0676af21dc0
Mancabm <mancabmor1@alum.us.es>
2023-03-25 13:36:25 +0100

    feat: Vista de detalles mental

---
+++ frontend/src/app/services/detalles-mental.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UsersService } from './users.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DetallesMentalService implements HttpInterceptor{
+
+  constructor(private http: HttpClient, private uService: UsersService) { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    req=req.clone({
+      withCredentials: true
+    });
+    return next.handle(req)
+  }
+
+  API_URL = 'http://isppgrupo9.pythonanywhere.com/'
+
+  getEntradaMental(id_entrada:any):Observable<any>{
+    if(this.uService.isLoggedIn()){
+      var ck = window.sessionStorage.getItem('auth-user')
+      if(ck!=null){
+        var tk = JSON.parse(ck);
+        var res = [];
+        for(var i in tk){
+          res.push(tk[i]);
+        }
+        let headers = new HttpHeaders()
+        headers = headers.set('Authorization', 'Token '+res[0])
+        return this.http.get(this.API_URL + `diary_entries/mental_entry/${id_entrada}/`, {'headers': headers})
+      }
+    }
+    return new Observable<any>
+  }
+}
