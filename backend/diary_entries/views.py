e18f1fa3c0024fcb4a7de8012885d073c580ebdb
Manuel Padilla Tabuenca <mpadillatabuenca@gmail.com>
2023-03-27 11:45:31 +0200

    FIX: Respuesta de modificación mental válida

--- backend/diary_entries/views.py
+++ backend/diary_entries/views.py
@@ -179,7 +179,7 @@ class MentalEntryId(APIView):
             
             entry.save()
 
-            return Response(PhysicalEntrySerializer(entry).data, status=status.HTTP_200_OK)
+            return Response(MentalEntrySerializer(entry).data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
