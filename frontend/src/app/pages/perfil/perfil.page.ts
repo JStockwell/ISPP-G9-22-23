781f76dc600ce2460589409538557b9ce1449c85
Álvaro Paradas <alvparbor1@alum.us.es>
2023-04-16 13:46:00 +0200

    vista de datos del perfil

---
+++ frontend/src/app/pages/perfil/perfil.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-perfil',
+    templateUrl: './perfil.page.html',
+    styleUrls: ['./perfil.page.scss'],
+})
+export class PerfilPage implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {
+        
+    }
+}
